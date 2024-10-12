519bcf9084fb5dc9488d89245bd946602fa77850
abbasmoosajee07 <abbas07moosajee@gmail.com>
2024-10-12 13:29:57 +0100

    Day 6

--- 2015/6/15Day6.py
+++ 2015/6/15Day6.py
@@ -34,29 +34,71 @@ for n in range(len(lights_inst)):
     instruction_matrix.append(instruction_table)
 instruction_matrix = np.array(instruction_matrix)
 
-
-# Create a binary matrix to represent all houes with lights 
-# All lights start turned off(0)
-light_matrix = np.zeros((1000, 1000)) 
+light_matrix_1 = np.zeros((1000, 1000)) 
 
 for n in range(len(lights_inst)):
     instruction_n = instruction_matrix[n]
     
     type = instruction_n[0]    
-    if type == "turn on":
-        action = 1
-    elif type == "turn off":
-        action = 0
-    else:
-        action = -1  # Need to absolute it
-    
+
     x1 = int(instruction_n[1])
     y1 = int(instruction_n[2])
     x2 = int(instruction_n[3])
     y2 = int(instruction_n[4])
-    print(f"x={x2-x1},y={y2-y1}")
     
+    dx = (x2 - x1) + 1
+    dy = (y2 - y1) + 1
     
-# toggle   = -1 and then absolute
-# turn on  == 1
-# turn off == 0
+    for xi in range(dx):
+        for yi in range(dy):
+            xn = x1 + xi
+            yn = y1 + yi
+            
+            if type == "turn on":
+                light_matrix_1[xn][yn] = 1
+            elif type == "turn off":
+                light_matrix_1[xn][yn] = 0
+            else:
+                light_matrix_1[xn][yn] = 1 - light_matrix_1[xn][yn] # Need to absolute it
+            
+        
+total_sum_1 = sum(element for row in light_matrix_1 for element in row)
+
+print("Total houses with lights on:", total_sum_1)  # Output: 45
+
+# Create a binary matrix to represent all houes with lights 
+# All lights start turned off(0)
+light_matrix_2 = np.zeros((1000, 1000)) 
+
+for n in range(len(lights_inst)):
+    instruction_n = instruction_matrix[n]
+    
+    type = instruction_n[0]    
+
+    x1 = int(instruction_n[1])
+    y1 = int(instruction_n[2])
+    x2 = int(instruction_n[3])
+    y2 = int(instruction_n[4])
+    
+    dx = (x2 - x1) + 1
+    dy = (y2 - y1) + 1
+    
+    for xi in range(dx):
+        for yi in range(dy):
+            xn = x1 + xi
+            yn = y1 + yi
+            
+            if type == "turn on":
+                light_matrix_2[xn][yn] = light_matrix_2[xn][yn] + 1
+            elif type == "turn off":
+                light_matrix_2[xn][yn] = light_matrix_2[xn][yn] - 1
+                if light_matrix_2[xn][yn] < 0:
+                    light_matrix_2[xn][yn] = 0
+            else:
+                light_matrix_2[xn][yn] = 2 + light_matrix_2[xn][yn] # Need to absolute it
+            
+        
+total_sum_2 = sum(element for row in light_matrix_2 for element in row)
+
+print("Total houses with lights on:", total_sum_2)  # Output: 45
+    
