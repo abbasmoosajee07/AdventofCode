66d571b2bf5a176248119d6b6c9ef1df1c92bffb
Abbas Moosajee <abbas07moosajee@gmail.com>
2024-11-20 04:41:18 +0000

    2020 Day 03 & Day 04

--- 2020/03/2020Day03.py
+++ 2020/03/2020Day03.py
@@ -2,7 +2,7 @@
 # Solution Started: Nov 19, 2024
 # Puzzle Link: https://adventofcode.com/2020/day/3
 # Solution by: [abbasmoosajee07]
-# Brief: [Code/Problem Description]
+# Brief: [Tree Map]
 
 #!/usr/bin/env python3
 
