c1ccfe0cd95f83f3e1e5981b093dae9b3b7aed29
Abbas Moosajee <abbas07moosajee@gmail.com>
2024-11-21 05:42:02 +0000

    minor change day 13

--- 2020/13/2020Day13.py
+++ 2020/13/2020Day13.py
@@ -2,7 +2,7 @@
 # Solution Started: Nov 21, 2024
 # Puzzle Link: https://adventofcode.com/2020/day/13
 # Solution by: [abbasmoosajee07]
-# Brief: [Code/Problem Description]
+# Brief: [Bus Timetable]
 
 #!/usr/bin/env python3
 
